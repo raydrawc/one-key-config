8569b7c2a291265e5c2959b1a2c975a7e6d3fec6
raydrawc <raydrawc@gmail.com>
2019-03-11 14:32:22 +0800

    [charge] update tmux quick open

--- tmux/tmuxquick.sh
+++ tmux/tmuxquick.sh
@@ -16,15 +16,14 @@ if [ -z $cmd ]; then
   exit 1
 fi
 
-$cmd has-session -t $session >/dev/null 2>&1      ## test seesion exit
+$cmd has-session -t $session >/dev/null 2>&1      # 测试会话存在
 if [ $? != 0 ]; then
     $cmd new-session -s  $session -n  $window -d -c "${work_path}"
     $cmd split-window -h  -t $session:$window.0 -c "${work_path}"
     $cmd split-window -v  -t $session:$window.0 -c "${work_path}"
-    $cmd send-keys -t $seesion:$window.2 "./dev.sh server start $work_platform $work_zone_id" C-m
-    $cmd send-keys -t $seesion:$window.1 "sleep 5s" C-m
-    $cmd send-keys -t $seesion:$window.1 "./dev.sh remsh  $work_platform $work_zone_id" C-m
-    $cmd send-keys -t $seesion:$window.0 "./dev.sh server mmake" C-m
+    $cmd send-keys -t $seesion:$window.0 "./dev.sh server mmake"     # 重新编译文件
+    $cmd send-keys -t $seesion:$window.2 "./dev.sh server start $work_platform $work_zone_id" C-m # 启动服务器
+    $cmd send-keys -t $seesion:$window.1 "sleep 15s; ./dev.sh remsh  $work_platform $work_zone_id" C-m  # 命令行工具接入
     $cmd select-pane -t $session:$window.0
 fi
 
