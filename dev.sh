ed734bc8fc4e334f622ef2fb21bbdab5d51fce08
raydrawc <raydrawc@gmail.com>
2020-12-28 03:00:24 +0800

    [修改] 初始化脚本

--- dev.sh
+++ dev.sh
@@ -4,6 +4,17 @@
 # @author raydraw@gmail.com
 # ----------------------------------
 
+# 获取脚本文件所在绝对路径(自动跟踪符号链接)
+get_real_path(){
+    local source="${BASH_SOURCE[0]}"
+    while [ -h "$source" ]; do
+        local dir="$( cd -P "$( dirname "$source" )" && pwd )"
+        source="$( readlink "$source" )"
+        [[ $source != /* ]] && source="$dir/$source"
+    done
+    echo "$( cd -P "$( dirname "$source" )/.." && pwd )"
+}
+
 # 调用函数
 _CALL_FUNCTION(){
     local fname="fun"
@@ -30,15 +41,26 @@ _CALL_FUNCTION(){
     done
 }
 
+# 输出命令行补全信息
+fun_completion(){
+    echo $(declare -p DOC)
+}
+
 # --- 执行入口 -----------------------------------
-ROOT=.
+ROOT=$(get_real_path)
 HOME=~
+
+# 设置帮助文本
+declare -A DOC
 
 # 额外载入工具脚本
 scripts=( )
 for f in ${scripts[@]}; do
-
-
+    if [ ! -f ${ROOT}/${f} ]; then
+        continue
+    fi
+    source ${ROOT}/${f}
+done
 
 # 根据参数调用相应命令
 _CALL_FUNCTION $@
