d327c83c0a812a1a72e9c6da973df327ef4318ff
raydrawc <raydrawc@163.com>
2021-03-25 00:23:40 +0800

    modif install

--- bash/install.sh
+++ bash/install.sh
@@ -12,17 +12,35 @@ function fun_install(){
 DOC[set_bash]="设置bash相关配置"
 function fun_set_bash() {
     today=`date +%Y%m%d`
-    cfg_path=$ROOT/bash/config/
-    for file in `ls $cfg_path` 
+    cfg_path=$ROOT/config/
+    cfgs=("bashrc" "myrc.sh" "profile")
+    for file in ${cfgs[@]};
     do
         filetmp=~/.$file
-        if [[ -e $filetmp ]]
+        if [[ -e $filetmp ]];
         then # 备份配置
-            mv -f $filetmp $filetmp.today
+            mv -f $filetmp $filetmp.$today
         fi
+        unlink $filetmp
         ln -s $cfg_path/$file $filetmp
     done
-    
+}
 
+
+DOC[set_git]="设置git相关配置"
+function fun_set_git() {
+    today=`date +%Y%m%d`
+    cfg_path=$ROOT/config/
+    cfgs=("gitconfig")
+    for file in ${cfgs[@]};
+    do
+        filetmp=~/.$file
+        if [[ -e $filetmp ]];
+        then # 备份配置
+            mv -f $filetmp $filetmp.$today
+        fi
+        unlink $filetmp
+        ln -s $cfg_path/$file $filetmp
+    done
 }
 
