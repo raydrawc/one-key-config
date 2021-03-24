fe42df63f8b424f0f9524090a830276acf7ee639
raydrawc <raydraw@163.com>
2021-03-24 16:31:41 +0800

    tmp

--- bash/install.sh
+++ bash/install.sh
@@ -7,5 +7,22 @@
 DOC[install]="辅助一键安装"
 function fun_install(){
     fun_set_bash
+}
+
+DOC[set_bash]="设置bash相关配置"
+function fun_set_bash() {
+    today=`date +%Y%m%d`
+    cfg_path=$ROOT/bash/config/
+    for file in `ls $cfg_path` 
+    do
+        filetmp=~/.$file
+        if [[ -e $filetmp ]]
+        then # 备份配置
+            mv -f $filetmp $filetmp.today
+        fi
+        ln -s $cfg_path/$file $filetmp
+    done
+    
 
 }
+
