de066f8a3ed40f21b9b8f179af8a7e2ee3b9e8fc
raydrawc <raydraw@163.com>
2021-12-23 15:32:57 +0800

    txy

---
+++ bash/install/fzf.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+#--------------------------------------
+# 安装工具
+# @author raydraw@163.com
+#--------------------------------------
+
+DOC[install_fzf]="fzf"
+function fun_install_fzf(){
+    sudo apt install fzf
+    sudo apt install fd-find
+    sudo apt install ripgrep
+}
+
+
